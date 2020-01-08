d40042a1d314e86ac6223ab9e6dc5dadd38031ea
prajwaldankit <prajwal.x82@gmail.com>
2020-01-08 14:11:24 +0545

    Updates

---
+++ precache-manifest.2b8bb6e8335656e853936fe105bcfc6a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48242070691bd24177fd8404f0a0ca59",
+    "url": "/user-api/index.html"
+  },
+  {
+    "revision": "ab4bedcc55e1e7cd6bcd",
+    "url": "/user-api/static/css/main.d40b18e8.chunk.css"
+  },
+  {
+    "revision": "9f08c909a1b22d84e9e1",
+    "url": "/user-api/static/js/2.c89dc7b9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/user-api/static/js/2.c89dc7b9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ab4bedcc55e1e7cd6bcd",
+    "url": "/user-api/static/js/main.c24c41a6.chunk.js"
+  },
+  {
+    "revision": "88a7e6a8b8956389af9d",
+    "url": "/user-api/static/js/runtime-main.1703eee9.js"
+  }
+]);
